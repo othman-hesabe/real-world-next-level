71789753d78ff367ee7d2ee45a1c387c9307b1c6
othman-hesabe <othman.hesabe@gmail.com>
2020-12-05 18:15:36 +0300

    axios progress bar

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const apiClient = axios.create({
     baseURL: 'http://localhost:3000',
@@ -9,9 +10,19 @@ const apiClient = axios.create({
     }
 })
 
+apiClient.interceptors.request.use(config =>{
+    NProgress.start()
+    return config
+})
+
+apiClient.interceptors.request.use(config =>{
+    NProgress.start()
+    return config
+})
+
 export default{
-    getEvents(){
-        return apiClient.get('/events')
+    getEvents(perPage, page){
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
     },
     getEvent(id){
         return apiClient.get('/events/' +id)
